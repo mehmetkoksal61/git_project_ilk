7b6cc0fe2f9dc20e6f0fe052e9af6a206f5c73ff
unknown <mehmetkoksal61@gmail.com>
2020-08-16 19:17:12 +0200

    future ilk version

--- src/git_project_ilk/BranchIlk.java
+++ src/git_project_ilk/BranchIlk.java
@@ -4,6 +4,7 @@ public class BranchIlk {
 
 	public static void main(String[] args) {
 		System.out.println("19:10 - master ilk version");
+		System.out.println("19:15 - future ilk version");
 
 	}
 
